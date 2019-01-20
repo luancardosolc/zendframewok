b8162434d69a0bb7d51f899c8ebd11f9d43a6d3c
unknown <luanleitecardoso@gmail.com>
2019-01-20 12:01:36 -0200

    Criando Factories

--- module/Pessoa/src/Module.php
+++ module/Pessoa/src/Module.php
@@ -10,4 +10,24 @@ class Module implements ConfigProviderInterface {
     {
         return include __DIR__.'/../config/module.config.php';
     }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\PessoaTable::class => function($container)
+                {
+                    $tableGateway = $container->get(Module\PessoaTableGateway::class);
+                    return new PessoaTable($tableGateway);
+                },
+                Model\PessoaTableGateway::class => function($container)
+                {
+                    $dbAdapter = $container->(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Pessoa());
+                    return new TableGateway('pessoa',$dbAdapter,null,$resultSetPrototype);
+                },
+            ]
+        ];
+    }
 }
