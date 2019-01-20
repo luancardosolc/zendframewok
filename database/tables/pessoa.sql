4c55a7e42d6dc2553cf09c3e829af70f64990fa7
unknown <luanleitecardoso@gmail.com>
2019-01-20 12:19:29 -0200

    Criando tabela pessoa no BD

---
+++ database/tables/pessoa.sql
@@ -0,0 +1,9 @@
+create table pessoa (
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(95),
+    sobrenome varchar(45),
+    email varchar(95),
+    situacao varchar(15)
+    );
+
+    
