5bebae8f718b99dab833eade8c0a4a828eac8911
unknown <luanleitecardoso@gmail.com>
2019-01-20 11:44:59 -0200

    Table Gateway da classe pessoa

---
+++ module/Pessoa/src/Model/PessoaTable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Pessoa\Model;
+
+use Zend\Db\TableGateway\TableGatewayInterface;
+use RuntimeException;
+
+class PessoaTable
+{
+    private $tableGateway;
+    
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function getAll()
+    {
+        return $this->tableGateway->select();
+    }
+
+    public function getPessoa($id)
+    {
+        $id = (int) $id;
+        $rowset = $this->tableGateway->select(['id' => $id]);
+        $row = $rowset->current();
+        if(!$row)
+        {
+            throw new RuntimeException(sprintf('Não foi encontrado o id %id',$id));
+        }
+        return $row;
+    }
+
+    public function salvarPessoa(Pessoa $pessoa)
+    {
+        $data = [
+            'id' => $pessoa->getId(),
+            'nome' => $pessoa->getNome(),
+            'sobrenome' => $pessoa->getSobrenome(),
+            'email' => $pessoa->getEmail(),
+            'situacao' => $pessoa->getSituacao()
+        ];
+
+        $id = (int) $pessoa->getId();
+        if($id === 0)
+        {
+            $this->tableGateway->insert($data);
+            return;
+        }
+        $this->tableGateway->update($data,['id'=>$id]);
+    }
+
+    public function deletarPessoa($id)
+    {
+        $this->tableGateway->delete(['id'=>(int)$id]);
+    }
+}
